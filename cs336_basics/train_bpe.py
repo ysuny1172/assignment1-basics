cdce3489defb50620860ff14dfdc84b2e2ce32dc
ysuny1172 <16329188+ysuny1172@user.noreply.gitee.com>
2026-05-23 22:01:52 +0800

    2

--- cs336_basics/train_bpe.py
+++ cs336_basics/train_bpe.py
@@ -280,22 +280,21 @@ def save_tokenizer_files(vocab, merges, out_dir):
             f.write(f"{s1} {s2}\n")
 
 def main():
-    input_path = "data/TinyStoriesV2-GPT4-valid.txt" # 你的原始文本路径
-    vocab_size = 1000 # 作业要求的词表大小
+    input_path = "../../data/TinyStoriesV2-GPT4-train.txt" # 你的原始文本路径
+    vocab_size = 10000 # 作业要求的词表大小
     # input_path = "data/owt_train.txt" 
     # input_path = "data/chinese.txt" 
     # vocab_size = 1000 # 作业要求的词表大小
     
     special_tokens = ["<|endoftext|>"]
-    output_dir = "data/TinyStoriesV2-GPT4-test"
+    output_dir = "../../data/TinyStoriesV2-GPT4"
 
     print(f"开始训练 BPE 分词器 (目标词表大小: {vocab_size})...")
     print("这可能需要几分钟，具体取决于你的 CPU 速度和倒排索引的效率。")
     
     # 调用你之前写好的逻辑
     vocab, merges = train_bpe(input_path, vocab_size, special_tokens)
-    
-    # 保存结果
+    print("BPE完成!")    # 保存结果
     save_tokenizer_files(vocab, merges, output_dir)
 
 if __name__ == "__main__":
